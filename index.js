44b24bdcebd06a35d3a99ecd3d091f7562947dd5
lokeshwarp <panugantilokeshwar2277@gmail.com>
2025-12-15 16:31:06 +0530

    all reset

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
-import userRoutes from "../routes/userroutes.js";
+import userRoutes from "./routes/userroutes.js";
 import mailRoutes from "./routes/mailroutes.js";
 
 dotenv.config();
